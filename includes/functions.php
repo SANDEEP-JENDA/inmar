2f12b412a0ea0f8101121c629fc69fe364da75f1
SANDEEP-JENDA <38372374+SANDEEP-JENDA@users.noreply.github.com>
2024-04-30 12:03:05 +0530

    added csrf

--- includes/functions.php
+++ includes/functions.php
@@ -7,4 +7,13 @@ function redirect_to($location) {
 function sanitize_input($input) {
     return htmlspecialchars(strip_tags($input));
 }
+function generate_csrf_token() {
+    if (!isset($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+}
+
+function validate_csrf_token($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
 ?>
